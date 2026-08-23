5bb277b5a3b31e9113967af4083046230d278be0
Muhammad Arshad <muhammadarshad.ieee@gmail.com>
2026-08-24 01:10:09 +0500

    Print Functions Completed

--- 01-basics/main.py
+++ 01-basics/main.py
@@ -1,3 +1,11 @@
 print("First Line!")
 print("Second Line!")
 print("Last Line!")
+
+language ="Python"
+print(language)
+
+version = 3.10
+language = "Python"
+print(language, version)
+print("The language is", language, "and the version is", version)
