622a920491d38a8df12b7122315fd4bba628c502
Muhammad Arshad <muhammadarshad.ieee@gmail.com>
2026-08-24 19:38:42 +0500

    Print Functions Completed

---
+++ 01-basics/math.py
@@ -0,0 +1,20 @@
+#Addition
+#x = 3
+#y = 2
+#print(x + y) #5
+#Subtraction
+#print(x - y) #1
+#Multiplication
+#print(x * y) #6
+#Division
+#print(x / y) #1.5
+#Floor Division
+#print(x // y) #1
+#Exponents
+#print(x ** y) #9
+#Remainder
+#print(x % y) #1
+
+#Order of Operations
+#op = 10 + ((10 * 2) / 5) ** 2 - 10
+#print(op) #16
--- 01-basics/variables.py
+++ 01-basics/variables.py
@@ -1,15 +1,15 @@
 #Creat a variable
-name = "Python"
-anotherName = "Python"
-multiStr = """
-Multi
-line
-string
-"""
-x = 1
-y = 1.5
-z = True
-print(name, anotherName, multiStr, x, y, z)
+#name = "Python"
+#anotherName = "Python"
+#multiStr = """
+#Multi
+#line
+#string
+#"""
+#x = 1
+#y = 1.5
+#z = True
+#print(name, anotherName, multiStr, x, y, z)
 #Make the variable name understandable
 #name = "Python"
 #Variable name rules
@@ -18,14 +18,44 @@ print(name, anotherName, multiStr, x, y, z)
 # 3. Variable names can only contain alphanumeric characters and underscores
 #nameOne = "Python"
 # 4. Variable names are case-sensitive
-Name = "Arshad"
-name = "Sehrish"
-print(Name, name)
+#Name = "Arshad"
+#name = "Sehrish"
+#print(Name, name)
 #A variable can contain varibales
 # x = 1
 # y = x + 1 #2
 #Operations for Variables
-x = 2
-y = 3
-z = x * y #6
-print(x, y, z)
+#x = 2
+#y = 3
+#z = x * y #6
+#print(x, y, z)
+#assign multiple values
+#player1, player2, player3 = "Arshad", "Sehrish", "Arshad1"
+#print(player1)
+#print(player2)
+#print(player3)
+#assign one value to many variables
+#player1 = player2 = player3 = player4 = "Arshad"
+#print(player1, player2, player3, player4)
+#printing variables
+#fruit = "Apple"
+#print("The fruit is:", fruit)
+#print("The fruit is:" + " " + fruit)
+#fruit1 = "Mango"
+#fruit2 = "Banana"
+#print("The fruit1 is", fruit1, "and the fruit2 is", fruit2)
+#print("The fruit1 is " + fruit1 + " and the fruit2 is " + fruit2)
+#You cannot combine strings and numbers
+#x = 20
+#print("The age is:" + x) #This will give an error
+#print("The age is:" + str(x)) #This will work
+#print("The age is:", x) #This will work
+#Get the type
+#x = 1
+#y = -5.3
+#fruit = "Apple"
+#numbers = [1, 2, 3, 4, 5]
+#print(type(x))
+#print(type(y))
+#print(type(fruit))
+#print(type(numbers))
