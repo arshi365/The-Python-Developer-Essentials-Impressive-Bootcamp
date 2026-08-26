07b70ccd3e819741f8e5c4a6cbc52b76b2868f8d
Muhammad Arshad <muhammadarshad.ieee@gmail.com>
2026-08-26 19:54:02 +0500

    Print Functions Completed

---
+++ 01-basics/lists.py
@@ -0,0 +1,32 @@
+#fruits = ["apple", "banana", "cherry", "date", "elderberry"] #List of Strings
+#print(fruit)
+#number = [1, 2, 3, 4, 5] #List of Integers
+#print(number)
+#print("The first fruit in the list is:", fruit[0]) #Accessing the first element of the list
+#print("The first number in the list is:", number[0]) #Accessing the first element of the list
+#print("The last number in the list is:", number[-1]) #Accessing the last element of the list
+#fruits [1] = "blueberry" #Modifying the second element of the list
+#print("The modified list of fruits is:", fruits) #Printing the modified list
+#fruits[0:2] = ["kiwi", "mango"] #Modifying the first two elements of the list
+#print("The modified list of fruits is:", fruits) #Printing the modified list
+#fruits.insert(2, "orange") #Inserting a new element at the third position of the list
+#print("The modified list of fruits is:", fruits) #Printing the modified list
+
+#Adding elements to the list
+#fruits.append("fig") #Adding a new element at the end of the list
+#print("The modified list of fruits is:", fruits) #Printing the modified list
+#newfruits = ["grape", "honeydew"] #Creating a new list of fruits
+#fruits.extend(newfruits) #Adding the new list of fruits to the existing list
+#print("The modified list of fruits is:", fruits) #Printing the modified list
+
+#Removing elements from the list
+#fruits.remove("banana") #Removing an element from the list
+#print("The modified list of fruits is:", fruits) #Printing the modified list
+#fruits.pop(1) #Removing an element from the list by index
+#print("The modified list of fruits is:", fruits) #Printing the modified list
+#del fruits[4] #Deleting an element from the list by index
+#print("The modified list of fruits is:", fruits) #Printing the modified list
+#del fruits #Deleting the entire list
+#print("The modified list of fruits is:", fruits) #Printing the modified list
+#fruits.clear() #Clearing all elements from the list
+#print("The modified list of fruits is:", fruits) #Printing the modified list
