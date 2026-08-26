b8acfafd8f2abfc2921836a284e44805ace1f336
Muhammad Arshad <muhammadarshad.ieee@gmail.com>
2026-08-26 13:12:57 +0500

    Print Functions Completed

---
+++ 01-basics/errors.py
@@ -0,0 +1,20 @@
+#SyntaxError
+#   print("Hello, World!")
+#IndexError
+#   my_list = [1, 2, 3]
+#   print(my_list[3])
+#KeyError
+#   my_dict = {"name": "John", "age": 30}
+#   print(my_dict["height"])
+#TypeError
+#   x = 5
+#   y = "hello"
+#   z = x + y
+#ValueError
+#   x = int("hello")
+#ImportError
+#   import non_existent_module
+#ModuleNotFoundError
+#   import non_existent_module
+#NameError
+#   print(undefined_variable)
